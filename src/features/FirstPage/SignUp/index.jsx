b59051f1fe7b6a1c3269db83a82e176bb342e202
James DivineMercy <iamoluchimercy@gmail.com>
2024-03-27 02:47:59 +0100

    updated

--- src/features/FirstPage/SignUp/index.jsx
+++ src/features/FirstPage/SignUp/index.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import style from "./index.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+
 
 const SignUp = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [message, setMessage] = useState('');
     const [usernameErrorMessage, setUsernameErrorMessage] = useState('');
-    const [passwordErrorMessage,setPasswordErrorMessage] = useState('');
+    const [passwordErrorMessage, setPasswordErrorMessage] = useState('');
+
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -15,9 +20,9 @@ const SignUp = () => {
             const response = await fetch('http://localhost:9491/api/v1/register', {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ username, password })
+                body: JSON.stringify({ username, password }),
             });
 
             if (response.ok) {
@@ -27,22 +32,24 @@ const SignUp = () => {
                 setPasswordErrorMessage('');
             } else {
                 const errorData = await response.json();
-
                 setMessage('');
-                if(errorData.error){
+                if (errorData.error) {
                     setUsernameErrorMessage(errorData.error);
                     setPasswordErrorMessage(" ");
-                }
-                else if(errorData.pError){
+                } else if (errorData.pError) {
                     setUsernameErrorMessage(" ");
                     setPasswordErrorMessage(errorData.pError);
                 }
-
             }
         } catch (error) {
             setUsernameErrorMessage(error.message);
             setPasswordErrorMessage(error.message);
         }
+    };
+
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
     };
 
     return (
@@ -51,17 +58,33 @@ const SignUp = () => {
                 <h1>Registration Form</h1>
                 <form onSubmit={handleSubmit}>
                     <div>
-                        <input type="text" placeholder="Enter your username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                        <input
+                            type="text"
+                            placeholder="Enter your username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
                         {usernameErrorMessage && <span className={style.usernameError}>{usernameErrorMessage}</span>}
                     </div>
                     <div>
-                        <input type="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                        {passwordErrorMessage && <span className={style.password}>{passwordErrorMessage}</span>}
+                        <div style={{display:"flex",flexDirection:"row"}}>
+                            <input
+                                type={showPassword ? "text" : "password"}
+                                placeholder="Enter your password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <button style={{marginLeft:"-50px",width:"50px",backgroundColor:"white",color:"black",marginTop:"18px",border:"none"}} onClick={togglePasswordVisibility} type="button">
+                                {showPassword ? <FontAwesomeIcon icon={faEyeSlash} /> : <FontAwesomeIcon icon={faEye} />}
+                            </button>
+                        </div>
+
+                        {passwordErrorMessage && <span className={style.passwordError}>{passwordErrorMessage}</span>}
                     </div>
                     <button type="submit">Submit</button>
                     <div className={style.exist}>
                         <span className={style.acct}>Already have an account?</span>
-                        <Link to="/signIn" style={{ color: "black" }}>
+                        <Link to="/signIn" style={{color: "black"}}>
                             <span className={style.login}>Login</span>
                         </Link>
                     </div>
